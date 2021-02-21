341bf7a97e549533c56ac39e6b8f14c6b04c5c85
Emily Schroeder <emilyschroeder20@gmail.com>
2021-02-21 12:16:18 -0600

    Added working navigation to info page

--- app/src/main/java/com/example/byitscover/InfoPage.java
+++ app/src/main/java/com/example/byitscover/InfoPage.java
@@ -4,12 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-public class FirstFragment extends Fragment {
+public class InfoPage extends Fragment {
 
     @Override
     public View onCreateView(
@@ -17,25 +18,27 @@ public class FirstFragment extends Fragment {
             Bundle savedInstanceState
     ) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+        return inflater.inflate(R.layout.enter_book_info, container, false);
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
+        view.findViewById(R.id.button_prev).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_from_first_to_review);
+                NavHostFragment.findNavController(InfoPage.this)
+                        .navigate(R.id.action_from_info_to_first);
             }
         });
 
-        view.findViewById(R.id.button_second).setOnClickListener(new View.OnClickListener() {
+        view.findViewById(R.id.Search).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_from_first_to_info);
+                EditText eTitle = (EditText) view.findViewById(R.id.enter_title);   //returns NULL
+                EditText eAuthor = (EditText) view.findViewById(R.id.enter_author); //returns NULL
+                NavHostFragment.findNavController(InfoPage.this)
+                        .navigate(R.id.action_from_info_to_review);
             }
         });
     }
