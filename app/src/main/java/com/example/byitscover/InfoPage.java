c797881ccceba64ce7c74ca8aa48cc658247717e
jrodonohue <jrodonohue@crimson.ua.edu>
2021-04-12 00:42:14 -0500

    Cleaned up InfoPage and removed dummy listings

--- app/src/main/java/com/example/byitscover/InfoPage.java
+++ app/src/main/java/com/example/byitscover/InfoPage.java
@@ -9,16 +9,12 @@ import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
-
 
 import com.example.byitscover.helpers.AggregateScraper;
 import com.example.byitscover.helpers.AsynchronousOperation;
-import com.example.byitscover.helpers.Book;
 import com.example.byitscover.helpers.BookListing;
 import com.example.byitscover.helpers.Query;
 import com.example.byitscover.helpers.Scraper;
-import com.example.byitscover.helpers.ScraperConstants;
 import com.example.byitscover.review_list_page.ReviewListPage;
 import com.example.byitscover.scrapers.BarnesAndNobleScraper;
 import com.example.byitscover.scrapers.GoodreadsScraper;
@@ -26,12 +22,8 @@ import com.example.byitscover.scrapers.GoogleScraper;
 import com.example.byitscover.scrapers.StorygraphScraper;
 
 import java.io.Serializable;
-import java.math.BigDecimal;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Callable;
 import java.util.concurrent.CancellationException;
 import java.util.concurrent.ExecutionException;
 
@@ -53,8 +45,8 @@ public class InfoPage extends Fragment {
     ) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.enter_book_info, container, false);
-        eTitle = (EditText) view.findViewById(R.id.enter_title); //text field to enter title
-        eAuthor = (EditText) view.findViewById(R.id.enter_author); //text field to enter author
+        eTitle = view.findViewById(R.id.enter_title); //text field to enter title
+        eAuthor = view.findViewById(R.id.enter_author); //text field to enter author
         return view;
     }
 
@@ -66,34 +58,6 @@ public class InfoPage extends Fragment {
         try {
             List<BookListing> listings;
             listings = scraperOperation.get();
-
-            listings = new ArrayList<BookListing>();
-
-            try {
-                listings.add(new BookListing(new URL("http://www.google.com/"),
-                        "something.com",
-                        new Book("A Modest Proposal", "Jonathan Swift", "Someone", null),
-                        null,
-                        null,
-                        null,
-                        null,
-                        BigDecimal.TEN));
-
-                listings.add(new BookListing(new URL("http://www.google.com/"),
-                        "b.com",
-                        new Book("Generic Book", "Generic Author", "Someone", null),
-                        null,
-                        null,
-                        null,
-                        null,
-                        BigDecimal.ONE));
-            } catch (MalformedURLException ex) {
-                ex.printStackTrace();
-            }
-
-            for (int i = 0; i < 10; i++) {
-                listings.add(listings.get(i % 2));
-            }
 
             Intent showReviewPage = new Intent(getActivity(), ReviewListPage.class);
             showReviewPage.putExtra(ReviewListPage.KEY_BOOK_LISTINGS, (Serializable)listings);
@@ -113,41 +77,35 @@ public class InfoPage extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.Search).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Cancel previous operation if user presses button multiple times
-                if (scraperOperation != null) {
-                    scraperOperation.cancel();
+        view.findViewById(R.id.Search).setOnClickListener(view1 -> {
+            // Cancel previous operation if user presses button multiple times
+            if (scraperOperation != null) {
+                scraperOperation.cancel();
 
-                    // If operation has already completed, then display the previous results
-                    if (!scraperOperation.isCancelled()) {
-                        return;
-                    }
+                // If operation has already completed, then display the previous results
+                if (!scraperOperation.isCancelled()) {
+                    return;
                 }
-
-                String title = eTitle.getText().toString();
-                String author = eAuthor.getText().toString();
-
-                Query query = new Query(title, author, null);
-
-                scraperOperation = new AsynchronousOperation<List<BookListing>>(
-                        new Callable<List<BookListing>>() {
-                            @Override
-                            public List<BookListing> call() throws Exception {
-                                List<Scraper> scrapers = new ArrayList<Scraper>();
-
-                                scrapers.add(new BarnesAndNobleScraper());
-                                scrapers.add(new GoodreadsScraper());
-                                scrapers.add(new GoogleScraper());
-                                scrapers.add(new StorygraphScraper());
-                                Scraper aggregate = new AggregateScraper(scrapers);
-
-                                return null;
-                            }
-                        },
-                        InfoPage.this::onScraperCompletion);
             }
+
+            String title = eTitle.getText().toString();
+            String author = eAuthor.getText().toString();
+
+            Query query = new Query(title, author, null);
+
+            scraperOperation = new AsynchronousOperation<>(
+                    () -> {
+                        List<Scraper> scrapers = new ArrayList<>();
+
+                        scrapers.add(new BarnesAndNobleScraper());
+                        scrapers.add(new GoodreadsScraper());
+                        scrapers.add(new GoogleScraper());
+                        scrapers.add(new StorygraphScraper());
+                        Scraper aggregate = new AggregateScraper(scrapers);
+
+                        return aggregate.scrape(query);
+                    },
+                    InfoPage.this::onScraperCompletion);
         });
     }
 
